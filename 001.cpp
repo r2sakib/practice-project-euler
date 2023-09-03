95b2d0468af08af4abb762d83aeb2f7231e8f19d
Sadman Sakib <sakibsadman035@gmail.com>
2023-09-03 22:04:54 +0600

    solved using for loop

--- 001.cpp
+++ 001.cpp
@@ -1,17 +1,14 @@
 #include<iostream>
 
 int main() {
-    long int sum;
-    int i = 1;
-    while (i < 1000) {
-        if (i % 3 == 0 or i % 5 == 0) {
+    int sum{0};
+    for (int i{0}; i < 1000; ++i) {
+        if (i % 5 == 0 or i % 3 == 0) {
             sum += i;
         };
+    };
 
-        i++;
-    }
-
-    std::cout << sum;
+    std::cout << &sum;
 
     return 0;
 }
